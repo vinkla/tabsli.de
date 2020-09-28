4e05cde5b00d40ae97b62b1e9e427b1619e6761d
Vincent Klaiber <vincent@doubledip.se>
2020-09-28 21:21:52 +0200

    Workin on copy feature

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,11 @@ export default function Home() {
         onChange={(event) => setValue(event.currentTarget.value)}
       />
       {theme === 'dark' ? (
-        <svg viewBox="0 0 20 20" onClick={() => setTheme('light')}>
+        <svg
+          className="theme"
+          viewBox="0 0 20 20"
+          onClick={() => setTheme('light')}
+        >
           <path
             fillRule="evenodd"
             d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
@@ -41,10 +45,22 @@ export default function Home() {
           ></path>
         </svg>
       ) : (
-        <svg viewBox="0 0 20 20" onClick={() => setTheme('dark')}>
+        <svg
+          className="theme"
+          viewBox="0 0 20 20"
+          onClick={() => setTheme('dark')}
+        >
           <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
         </svg>
       )}
+      <svg
+        className="copy"
+        viewBox="0 0 20 20"
+        onClick={() => console.log(btoa(encodeURIComponent(value)))}
+      >
+        <path d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z" />
+        <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
+      </svg>
     </>
   );
 }
