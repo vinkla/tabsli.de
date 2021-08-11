69afe0da38c7422e08e535cbbdb36b44f275c1a9
Vincent Klaiber <vincent@doubledip.se>
2021-08-11 09:34:52 +0200

    Added todos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import App from "./App.svelte";
 
 // TODO: Add first letter of text as favicon.
+// TODO: Adjust font size based on viewport width.
+// TODO: Add multiline support with text alignment support.
 
 const app = new App({
   target: document.body,
